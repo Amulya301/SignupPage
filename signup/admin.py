3fb7295ae83e674c510ec48bb15940bfabeeb355
vedvund <vedvund@gmail.com>
2022-03-19 10:31:16 +0530

    resolved

--- signup/admin.py
+++ signup/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
 from signup.models import MyModel
+
 # Register your models here.
 admin.site.register(MyModel)
--- signup/views.py
+++ signup/views.py
@@ -6,30 +6,32 @@ from django.forms import modelform_factory
 from django.core.exceptions import ValidationError
 from django.contrib.messages.api import success
 from django.contrib import messages
+
 # Create your views here.
 
 userform = modelform_factory(MyModel, exclude=[])
+
+
 def home(request):
     return HttpResponse('hello')
 
 
-
 def signup(request):
-
     if request.method == "POST":
 
         details = userform(request.POST)
         if details.is_valid():
-           details.save()
-           return HttpResponse('User details has been submitted successfully')
+            details.save()
+            return HttpResponse('User details has been submitted successfully')
         else:
             form = userform()
-            print('User already exists')
-            return render(request,'signup/signup.html',{
-                "form" : form
+            return render(request, 'signup/signup.html', {
+                "form": form,
+                "error_message": "User Already Exists"
             })
+
     else:
         form = userform()
-        return render(request,'signup/signup.html',{
-            "form" : form
+        return render(request, 'signup/signup.html', {
+            "form": form
         })
